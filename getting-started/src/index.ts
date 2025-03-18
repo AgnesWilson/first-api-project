010d5c907988330a642bc1f0e0521eeffb62b039
AgnesWilson <agnes.wilson97@hotmail.com>
2025-03-18 18:23:46 +0100

    ✨ added toDos

--- getting-started/src/index.ts
+++ getting-started/src/index.ts
@@ -1,7 +1,35 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 const app = express();
+
+import { Todo } from './ToDos';
 
 const PORT = 3000
 app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`)
 });
+
+app.get('/', (_: Request, res: Response) => {
+    res.send('Hello World')
+  })
+
+const todos: Todo[] = [
+    new Todo('Gå på promenad'),
+    new Todo('Blötlägg ris'),
+    new Todo('Värm middag'),
+    new Todo('Koka ris'),
+    new Todo('Ladda ner TV spel'),
+    new Todo('Gör klart skolarbete'),
+]
+
+app.get('/todos', (req: Request, res: Response) => {
+    res.json(todos)
+
+    // const search = req.query.search
+    // const sort = req.query.sort
+
+    // let filteredToDos = todos;
+
+    // if (search) {
+    //     filteredToDos = filteredToDos.filter((t) => t.content.includes(search.toString()))
+    // }
+})
