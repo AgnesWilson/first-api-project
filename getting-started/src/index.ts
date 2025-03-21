9e656d9b6e958766ed3b9db266769de520ccfe4e
AgnesWilson <agnes.wilson97@hotmail.com>
2025-03-21 16:20:43 +0100

    🚚 Refractor - move routing to separate routing file

--- getting-started/src/index.ts
+++ getting-started/src/index.ts
@@ -1,17 +1,13 @@
-import express, { Request, Response } from 'express';
-import { fetchAllPosts, searhPostsById, createPost, editPost, deletePost } from './controller/postsController';
+import express from 'express';
+import routing from './routes/postRouting'
 
 const app = express();
 
 // Middleware - gäller för alla requests
 app.use(express.json()) // Hanterar omvandling från json till js/ts så att vi kan hantera det
 
-app.get('/posts', fetchAllPosts)
-app.get('/posts/:id', searhPostsById)
-// ------- LEKTION NUMMER TVÅ ------- //
-app.post('/posts', createPost)
-app.patch('/posts/:id', editPost)
-app.delete('/posts/:id', deletePost)
+//Routing 
+app.use('/posts', routing)
 
 const PORT = 3000
 app.listen(PORT, () => {
