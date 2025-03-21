0a91448c0f014cdc0a9ef28e33b9e3e060ed2e59
AgnesWilson <agnes.wilson97@hotmail.com>
2025-03-21 16:24:27 +0100

    ➕ Installed corse

--- getting-started/src/index.ts
+++ getting-started/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors'
+
 import routing from './routes/postRouting'
 
 const app = express();
 
 // Middleware - gäller för alla requests
 app.use(express.json()) // Hanterar omvandling från json till js/ts så att vi kan hantera det
+app.use(cors());
 
 //Routing 
 app.use('/posts', routing)
